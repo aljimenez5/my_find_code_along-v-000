abf8fa0541cdb323a4b3c2abd6d33d8a48779e04
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 01:42:07 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,7 @@ def my_find(collection)
   index_count = 0
   new_collection = []
   while index_count < collection.length
-    if yield(collection[index_count])
-      return collection[index_count]
+    return collection[index_count] if yield(collection[index_count])
     end
     index_count += 1 
   end
